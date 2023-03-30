6aa7a6c9be0855adf7017d96a8ed293bf777abfd
matheushenriquefs <matheus.henrique1021@gmail.com>
2023-03-30 11:48:30 -0300

    refactor: show or hide content from columns based on props

--- src/modules/chat/components/ChatLayout/ChatLayout.vue
+++ src/modules/chat/components/ChatLayout/ChatLayout.vue
@@ -1,12 +1,34 @@
+<script setup lang="ts">
+type Props = {
+  visibility?: {
+    column1: boolean
+    column2: boolean
+  }
+}
+const props = withDefaults(defineProps<Props>(), {
+  visibility: () => ({
+    column1: true,
+    column2: true
+  })
+})
+</script>
+
 <template>
   <main class="container-fluid">
     <div class="row">
-      <div class="col-lg-4">
+      <div class="col-lg-4 pl-0 pr-0" v-if="props.visibility.column1">
         <slot name="column-1"></slot>
       </div>
-      <div class="col-lg-8">
+      <div class="col-lg-8 pl-0 pr-0" v-if="props.visibility.column2">
         <slot name="column-2"></slot>
       </div>
     </div>
   </main>
 </template>
+
+<style lang="scss" scoped>
+main,
+.row {
+  height: 100%;
+}
+</style>
