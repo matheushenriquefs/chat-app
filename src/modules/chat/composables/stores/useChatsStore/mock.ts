22300d5110f609eaff8022ec6fda6edb026e0ffc
matheushenriquefs <matheus.henrique1021@gmail.com>
2023-04-03 19:28:05 -0300

    feat(chat): update mock to meet new type requirements

--- src/modules/chat/composables/stores/useChatsStore/mock.ts
+++ src/modules/chat/composables/stores/useChatsStore/mock.ts
@@ -1,4 +1,6 @@
-export const mock = [
+import type { Chat } from '@/modules/chat/types/Chat'
+
+export const mock: Chat[] = [
   {
     id: 1,
     others: [
@@ -6,6 +8,16 @@ export const mock = [
         username: 'John Doe',
         profilePicture: 'https://picsum.photos/id/454/48'
       }
-    ]
+    ],
+    messages: [
+      {
+        id: 1,
+        content:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in nisl et odio tincidunt imperdiet.',
+        type: 'text',
+        date: new Date().toISOString()
+      }
+    ],
+    type: 'chat'
   }
 ]
