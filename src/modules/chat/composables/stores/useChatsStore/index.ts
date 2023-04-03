ee781778db87fbc6cc1199efb08f941955e2aa59
matheushenriquefs <matheus.henrique1021@gmail.com>
2023-04-03 19:28:41 -0300

    feat(chat): set ref type and increase timeout

--- src/modules/chat/composables/stores/useChatsStore/index.ts
+++ src/modules/chat/composables/stores/useChatsStore/index.ts
@@ -2,13 +2,14 @@ import { ref } from 'vue'
 import type { Ref } from 'vue'
 import { defineStore } from 'pinia'
 
+import type { Chat } from '@/modules/chat/types/Chat'
 import { mock } from './mock'
 
 export const useChatsStore = defineStore('chats', () => {
-  const chats: Ref<typeof mock> = ref([])
+  const chats: Ref<Chat[]> = ref([])
 
   // TODO: Get chats from API
-  setTimeout(() => chats.value = mock, 250)
+  setTimeout(() => (chats.value = mock), 500)
 
   return { chats }
 })
