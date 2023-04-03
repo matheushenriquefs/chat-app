97bf397506b37ffaad992044fa5a710be3bd4818
matheushenriquefs <matheus.henrique1021@gmail.com>
2023-04-03 11:22:55 -0300

    chore: set mocked data

--- src/modules/chat/composables/stores/useChatsStore/index.ts
+++ src/modules/chat/composables/stores/useChatsStore/index.ts
@@ -1,8 +1,14 @@
 import { ref } from 'vue'
+import type { Ref } from 'vue'
 import { defineStore } from 'pinia'
 
+import { mock } from './mock'
+
 export const useChatsStore = defineStore('chats', () => {
-  const chats = ref([])
+  const chats: Ref<typeof mock> = ref([])
+
+  // TODO: Get chats from API
+  setTimeout(() => chats.value = mock, 250)
 
   return { chats }
 })
