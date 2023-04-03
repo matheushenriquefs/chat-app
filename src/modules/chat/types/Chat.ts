5c084e56d94832772bc5a6cfb1ed9af32e45d79a
matheushenriquefs <matheus.henrique1021@gmail.com>
2023-04-03 19:27:28 -0300

    feat(chat): add global types for chat related resources

---
+++ src/modules/chat/types/Chat.ts
@@ -0,0 +1,19 @@
+export type ChatMessage = {
+  id: number
+  content: string
+  type: 'text'
+  date: string
+  seenAt?: string
+}
+
+type ChatMember = {
+  username: string
+  profilePicture: string
+}
+
+export type Chat = {
+  id: number
+  others: ChatMember[]
+  messages: ChatMessage[]
+  type: 'chat' | 'group'
+}
