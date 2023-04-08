fc442e75efb31bb617fba2b1ea2f0bd3577e838c
matheushenriquefs <matheus.henrique1021@gmail.com>
2023-04-08 00:21:15 -0300

    feat(chat): add component to serve as container to content for chat conversation

---
+++ src/modules/chat/components/ChatBody/ChatBody.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="chat-body">
+    <div class="chat-body-content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.chat-body {
+  flex-grow: 1;
+  height: 100%;
+  order: 2;
+  position: relative;
+  width: 100%;
+
+  &-content {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+    justify-content: flex-end;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    padding: var(--spacing);
+    position: absolute;
+    width: 100%;
+  }
+}
+</style>
---
+++ src/modules/chat/components/ChatBody/index.ts
@@ -0,0 +1,3 @@
+import ChatBody from './ChatBody.vue'
+
+export { ChatBody }
