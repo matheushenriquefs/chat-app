0dd268577bf03163ec5b0735f04dbc39df83c8fb
matheushenriquefs <matheus.henrique1021@gmail.com>
2023-04-08 00:04:58 -0300

    feat(chat): add component that bind message template types to component

---
+++ src/modules/chat/components/ChatMessages/ChatMessages.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { ChatTextMessage } from '@/modules/chat/components/ChatTextMessage'
+import type { ChatMessage } from '@/modules/chat/types/Chat'
+
+const props = defineProps<{
+  messages: ChatMessage[]
+}>()
+
+const templates = {
+  text: ChatTextMessage
+}
+</script>
+
+<template>
+  <div>
+    <component
+      v-for="message of props.messages"
+      :key="message.id"
+      :is="templates[message.type]"
+      :message="message"
+    ></component>
+  </div>
+</template>
