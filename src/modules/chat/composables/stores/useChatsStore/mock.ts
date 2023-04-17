d2528ca56b54929ec0b57c91a4a7a99e71da4fdc
matheushenriquefs <matheus.henrique1021@gmail.com>
2023-04-17 19:49:50 -0300

    chore: update mock

--- src/modules/chat/composables/stores/useChatsStore/mock.ts
+++ src/modules/chat/composables/stores/useChatsStore/mock.ts
@@ -15,13 +15,14 @@ export const mock: Chat[] = [
         content:
           'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam in nisl et odio tincidunt imperdiet.',
         type: 'text',
-        date: '2023-04-14T15:10:10.038Z'
+        createdAt: '2023-04-14T15:10:10.038Z',
+        seenAt: '2023-04-14T15:10:10.038Z'
       },
       {
         id: 2,
         content: 'Hello world.',
         type: 'text',
-        date: '2023-04-14T15:11:10.038Z'
+        createdAt: '2023-04-14T15:11:10.038Z'
       }
     ],
     type: 'chat',
@@ -40,13 +41,40 @@ export const mock: Chat[] = [
         id: 3,
         content: 'Hello world.',
         type: 'text',
-        date: '2023-04-14T15:12:10.038Z'
+        createdAt: '2023-04-14T15:12:10.038Z'
       },
       {
         id: 4,
         content: 'World Hello.',
         type: 'text',
-        date: '2023-04-14T15:12:30.038Z'
+        createdAt: '2023-04-14T15:12:30.038Z'
+      }
+    ],
+    type: 'chat',
+    isActive: false
+  },
+  {
+    id: 3,
+    members: [
+      {
+        username: 'Steve Perry',
+        profilePicture: 'https://picsum.photos/id/456/48'
+      }
+    ],
+    messages: [
+      {
+        id: 3,
+        content: 'Hello Perry.',
+        type: 'text',
+        createdAt: '2023-04-14T15:12:10.038Z',
+        seenAt: '2023-04-14T15:12:10.038Z'
+      },
+      {
+        id: 4,
+        content: 'World Hello Steve.',
+        type: 'text',
+        createdAt: '2023-04-14T15:12:30.038Z',
+        seenAt: '2023-04-14T15:12:30.038Z'
       }
     ],
     type: 'chat',
