ab17efc42c9eeb80e1a1fe87bada4a14361176aa
matheushenriquefs <matheus.henrique1021@gmail.com>
2023-04-17 17:32:14 -0300

    feat(chat): isolate logic of get last message displaying from chat so its more testable

---
+++ src/modules/chat/composables/chat/useChatLastMessage.ts
@@ -0,0 +1,41 @@
+import { ref, unref, isRef, watchEffect } from 'vue'
+import type { Ref } from 'vue'
+import type { MaybeRef } from '@vueuse/core'
+
+import type { ChatMessage } from '@/modules/chat/types/Chat'
+
+export const useChatLastMessage = (
+  messages: MaybeRef<ChatMessage[]>
+): {
+  lastMessage: Ref<ChatMessage | null>
+} => {
+  const message: Ref<ChatMessage | null> = ref({
+    id: 0,
+    content: '',
+    type: 'text' as 'text',
+    date: ''
+  })
+
+  function doFormat() {
+    message.value = {
+      id: 0,
+      content: '',
+      type: 'text',
+      date: ''
+    }
+
+    const unrefed = unref(messages)
+
+    const reversedMessages = [...unrefed].reverse()
+
+    reversedMessages.length ? (message.value = reversedMessages[0]) : (message.value = null)
+  }
+
+  if (isRef(messages)) {
+    watchEffect(doFormat)
+  } else {
+    doFormat()
+  }
+
+  return { lastMessage: message }
+}
