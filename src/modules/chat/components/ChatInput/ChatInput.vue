380fafc9731fc61001eeccdc50eab9effd4f5352
matheushenriquefs <matheus.henrique1021@gmail.com>
2023-05-19 15:54:56 -0300

    refactor: use composable instead

--- src/modules/chat/components/ChatInput/ChatInput.vue
+++ src/modules/chat/components/ChatInput/ChatInput.vue
@@ -1,49 +1,29 @@
 <script setup lang="ts">
-import { ref, onMounted } from 'vue'
 import type { Ref } from 'vue'
+import { useTextareaAutosize } from '@vueuse/core'
 
-const message: Ref<string> = ref('')
-const textAreaElement: Ref<HTMLTextAreaElement | null> = ref(null)
 const cssVariables = {
-  spacing: window.getComputedStyle(document.documentElement).getPropertyValue('--spacing'),
-  lineHeight: window.getComputedStyle(document.documentElement).getPropertyValue('--line-height'),
-  spacingVertical: window
-    .getComputedStyle(document.documentElement)
-    .getPropertyValue('--form-element-spacing-vertical'),
-  borderWidth: window.getComputedStyle(document.documentElement).getPropertyValue('--border-width')
+  spacing: window.getComputedStyle(document.documentElement).getPropertyValue('--spacing')
 }
-
-const setStyles = (): void => {
-  if (textAreaElement.value) {
-    const parentElement = textAreaElement.value.parentElement as HTMLLabelElement
-
-    if (!message.value) {
-      const style = `height: calc(
-          1rem * ${cssVariables.lineHeight} + ${cssVariables.spacingVertical} * 2 + ${cssVariables.borderWidth} * 2
-        ); max-height: calc(${cssVariables.spacing} * 8);`
-      textAreaElement.value.setAttribute('style', style)
-      parentElement.setAttribute('style', style)
-
-      return
-    }
-
-    const style = `height: calc(${textAreaElement.value.scrollHeight}px + 2px); max-height: calc(${cssVariables.spacing} * 8);`
-    textAreaElement.value.setAttribute('style', style)
-    parentElement.setAttribute('style', style)
-  }
+const onResize = (textareaRef: Ref<HTMLTextAreaElement>) => {
+  const style = `height: calc(${textarea.value.clientHeight}px + 4px); max-height: calc(${cssVariables.spacing} * 8);`
+  const parentElement = textareaRef.value.parentElement as HTMLLabelElement
+  textareaRef.value.setAttribute('style', style)
+  parentElement.setAttribute('style', style)
 }
+const { textarea, input } = useTextareaAutosize({
+  onResize: () => onResize(textarea)
+})
 
-const handleInput = (): void => setStyles()
-
-onMounted(() => setStyles())
+const handleInput = () => {}
 </script>
 
 <template>
   <label for="message">
     <textarea
       id="message"
-      ref="textAreaElement"
-      v-model.trim="message"
+      ref="textarea"
+      v-model.trim="input"
       name="message"
       @input="handleInput"
     ></textarea>
@@ -57,9 +37,6 @@ label {
 }
 
 textarea {
-  height: calc(
-    1rem * var(--line-height) + var(--form-element-spacing-vertical) * 2 + var(--border-width) * 2
-  );
   margin-bottom: 0;
   margin-top: 0;
   padding: var(--form-element-spacing-vertical) var(--form-element-spacing-horizontal);
